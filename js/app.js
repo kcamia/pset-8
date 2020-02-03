911db788e30aa17a320f635c75eb1fd78f2dd993
Kristina Camia <kcamia19@gmail.com>
2020-02-03 12:41:51 -0500

    taking turns 5

--- js/app.js
+++ js/app.js
@@ -1,17 +1,31 @@
 ///////////////////// CONSTANTS /////////////////////////////////////
 
+const winningConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
 ///////////////////// APP STATE (VARIABLES) /////////////////////////
 
 let board;
 let turn;
+let win;
 
 ///////////////////// CACHED ELEMENT REFERENCES /////////////////////
 
 const squares = Array.from(document.querySelectorAll("#board div"));
+const message = document.querySelector("h2");
 
 ///////////////////// EVENT LISTENERS ///////////////////////////////
 
 window.onload = init;
+document.getElementById("board").onclick = takeTurn;
 
 ///////////////////// FUNCTIONS /////////////////////////////////////
 
@@ -21,12 +35,46 @@ function init() {
     "", "", "",
     "", "", ""
   ];
+  turn = "X";
+  win = null;
 
-  render();   // we'll write this later
+  render();
 }
 
 function render() {
   board.forEach(function(mark, index) {
     squares[index].textContent = mark;
   });
+
+  message.textContent = win ? `${win} wins!` : `Turn: ${turn}`;
+}
+
+function takeTurn(e) {
+  if (!win) {
+    let index = squares.findIndex(function(square) {
+      return square === e.target;
+    });
+
+    board[index] = turn;
+    turn = turn === "X" ? "O" : "X";
+    win = getWinner();
+
+    render();
+  }
+}
+
+function getWinner() {
+  let winner = null;
+
+  winningConditions.forEach(function(condition, index) {
+    if (
+      board[condition[0]] &&
+      board[condition[0]] === board[condition[1]] &&
+      board[condition[1]] === board[condition[2]]
+    ) {
+      winner = board[condition[0]];
+    }
+  });
+
+  return winner;
 }
