677cd0cf46e8475fa327979b0c993e307b777a0f
Kristina Camia <kcamia19@gmail.com>
2020-02-03 12:33:48 -0500

    x and o 4

--- js/app.js
+++ js/app.js
@@ -2,8 +2,31 @@
 
 ///////////////////// APP STATE (VARIABLES) /////////////////////////
 
+let board;
+let turn;
+
 ///////////////////// CACHED ELEMENT REFERENCES /////////////////////
+
+const squares = Array.from(document.querySelectorAll("#board div"));
 
 ///////////////////// EVENT LISTENERS ///////////////////////////////
 
+window.onload = init;
+
 ///////////////////// FUNCTIONS /////////////////////////////////////
+
+function init() {
+  board = [
+    "", "", "",
+    "", "", "",
+    "", "", ""
+  ];
+
+  render();   // we'll write this later
+}
+
+function render() {
+  board.forEach(function(mark, index) {
+    squares[index].textContent = mark;
+  });
+}
